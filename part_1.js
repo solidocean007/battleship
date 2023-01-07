5be6892cb1dbe51e49804970b54d26cacffa3bad
solidocean007 <clintonwilliams007@gmail.com>
2023-01-07 13:03:53 -0500

    Add a gitignore file

---
+++ part_1.js
@@ -0,0 +1,19 @@
+
+var readlineSync = require('readline-sync');
+
+// Wait for user response.
+
+if (readlineSync.keyIn('Press any key to start the game.')) {
+  
+}
+
+// Place enemy ships on the board
+
+function buildGrid(){
+  
+}
+let xCoord = Math.floor(Math.random() * (3) + 1)
+let yCoord = Math.floor(Math.random() * (3) + 1)
+console.log(xCoord, yCoord)
+
+
