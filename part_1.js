3bbf3508e0d4f2038d2bdd948e139ff33e764e10
solidocean007 <clintonwilliams007@gmail.com>
2023-01-13 05:57:36 -0500

    Building logic for part-1

--- part_1.js
+++ part_1.js
@@ -1,118 +1,117 @@
+const boardSize = 3;
+const numberOfFoes = 2;
+const boardLetters = ['A','B','C','D','E','F','G','H','I','J'];
+const enemyBoats = [];
+const boardValues = ['1','2','3','4','5','6','7','8','9','10'];
 
-var readlineSync = require('readline-sync');
 
-// Wait for user response.
 
-if (readlineSync.keyIn('Press any key to start the game.   ')) {
-  
+// this function creates enemy boats and assigns them a letter and a number.
+function createEnemyTargets(){
+  const enemyBoats = [];
+  for( let shipCount = 0; shipCount < numberOfFoes; shipCount++) {
+    let xCoord = Math.floor(Math.random() * (boardSize) + 1)
+    xCoord === 1 ? xCoord = 'A'
+            :xCoord === 2 ? xCoord = 'B'
+            :xCoord === 3 ? xCoord = 'C'
+            :xCoord === 4 ? xCoord = 'D'
+            :xCoord === 5 ? xCoord = 'E'
+            :xCoord === 6 ? xCoord = 'F'
+            :xCoord === 7 ? xCoord = 'G'
+            :xCoord === 8 ? xCoord = 'H'
+            :xCoord === 9 ? xCoord = 'I'
+            :xCoord = 'J'
+
+      let yCoord = Math.floor(Math.random() * (3) + 1)
+      enemyBoats[shipCount] = String((xCoord + yCoord)); 
+    }
+    return String(enemyBoats);
+    
 }
 
-// Place enemy ships on the board
-
+// This function creates a board.
 function buildGrid(size){
   // create board pieces without middle block
   const lineOnTop = ' ______ ';
   const boxSpace = '|      |';
   // no middle block
   const bottom = `|______|`;
-  
   // create the length of the pieces without the middle block
   const topLine = lineOnTop.repeat(size);
   const nextLine = boxSpace.repeat(size);
   
-  // Create data plots for board.
-  let gameBoardData = [];
-  const letters = 'ABCDEFGHIJ';                               // declare letter string for x coordinates                              // begin cycling through letters
-  const values = ['1','2','3','4','5','6','7','8','9','10'];
 
-  for(let i = 0; i < size; i++) {
-    let xChar = letters[i];
-    for(let j = 0; j < size; j++) {
-      let yNumber = values[j];
-      gameBoardData.push({[[xChar]+[yNumber]]: null})
+   for(let xPlotter = 0; xPlotter < size; xPlotter++) {    //setup loop the length of the board
+    console.log(topLine);
+    console.log(nextLine);
+    let string = '';
+    for(let i = 0; i < size; i++){   
+      const letters = 'ABCDEFGHIJ';                               // declare letter string for x coordinates                              // begin cycling through letters
+      letterValue = letters[xPlotter];                            // grab a letter
+      const board = [];                                             // initialize box plot container
+      board[xPlotter] = { letterValue : xPlotter}
+
+      string += `|      |`;
+    }
+    console.log(string);
+
+    let bottomLine = bottom.repeat(size);
+    console.log(bottomLine);
+  }
+}
+
+ // This function builds and array of objects that represent the plots of the board.
+ function gameBoardData(boardSize){
+  let gameBoardData = [];
+  for(let i = 0; i < boardSize; i++) {
+    let xChar = boardLetters[i];
+    for(let j = 0; j < boardSize; j++) {
+      let yNumber = boardValues[j];
+      gameBoardData.push(xChar+yNumber)
     }
   }
-  console.log(gameBoardData);
+  return gameBoardData;
+}
+// console.log([gameBoardData(boardSize)]);
+
+
+var readlineSync = require('readline-sync');
+// Wait for user response.
+if (readlineSync.keyIn('Press any key to start the game.   ')) {
 }
 
-const boardSize = 10;
+// Draw the board in the terminal.
 console.log(buildGrid(boardSize));
 
-const numberOfEnemy = 2;
+// Create the enemy ships 
+// console.log(createEnemyTargets());
+createEnemyTargets();
 
-for( let targets = 0; targets < numberOfEnemy; targets++) {
-  let xCoord = Math.floor(Math.random() * (boardSize) + 1)
-  console.log(xCoord);
+// console.log([enemyBoats]);  // why doesn't this log the enemy boats?
 
-// switch (xCoord) {
-//   case 1:
-//     xCoord = 'A';
-//     break;
-//   case 2:
-//     xCoord = 'B';
-//     break;
-//   case 3:
-//     xCoord = 'C';
-//     break;
-//   case 4:
-//     xCoord = 'D';
-//     break;
-//   case 5:
-//     xCoord = 'E';
-//     break;
-//   case 6:
-//     xCoord = 'F';
-//     break;
-//   case 7:
-//     xCoord = 'G';
-//     break;
-//   case 8:
-//     xCoord = 'H';
-//     break;
-//   case 9:
-//     xCoord = 'I';
-//     break;
-//   case 10:
-//     xCoord = 'J';
-//     break;
-// }
+const entries = Object.entries(enemyBoats);
+console.log('entries: ' + entries)
 
- xCoord === 1 ? xCoord = 'A'
-        :xCoord === 2 ? xCoord = 'B'
-        :xCoord === 3 ? xCoord = 'C'
-        :xCoord === 4 ? xCoord = 'D'
-        :xCoord === 5 ? xCoord = 'E'
-        :xCoord === 6 ? xCoord = 'F'
-        :xCoord === 7 ? xCoord = 'G'
-        :xCoord === 8 ? xCoord = 'H'
-        :xCoord === 9 ? xCoord = 'I'
-        :xCoord = 'J'
+readlineSync.setDefaultOptions({limit: boardLetters});
+xAnswer = (readlineSync.question('Enter a location to strike ie "A2" A - J')).toLocaleUpperCase();
 
-  let yCoord = Math.floor(Math.random() * (3) + 1)
+readlineSync.setDefaultOptions({limit: boardValues});
+yAnswer = readlineSync.question('1 - 10');
 
-  const firstEnemyShip = ({[xCoord + yCoord]: null}); 
-  console.log(firstEnemyShip);
-  console.log(`Testing: Enemy Ship is at: ${[firstEnemyShip]}.`);
+let playerShot = String([xAnswer + yAnswer])
+console.log(playerShot);
+console.log('Type of playerShot is: '+ typeof playerShot);
+console.log(typeof enemyBoats);
+
+
+if(enemyBoats.includes(playerShot)){
+  console.log('Hit and sunk!')
+} else {
+  console.log('miss me ha ha!');
 }
 
+// ** But `promptCL` method should be used instead of this. **
 
-  // for(let xPlotter = 0; xPlotter < size; xPlotter++) {    //setup loop the length of the board
-  //   console.log(topLine);
-  //   console.log(nextLine);
-  //   let string = '';
-  //   for(let i = 0; i < size; i++){   
-  //     const letters = 'ABCDEFGHIJ';                               // declare letter string for x coordinates                              // begin cycling through letters
-  //     letterValue = letters[xPlotter];                            // grab a letter
-  //     const board = [];                                             // initialize box plot container
-  //     board[xPlotter] = { letterValue : xPlotter}
-  //     string += `|  ${board[xPlotter]}  |`;
-  //   }
-  //   console.log(string);
-
-  //   let bottomLine = bottom.repeat(size);
-  //   console.log(bottomLine);
-  // }
-  
   // for(i = 0; i < boardLength; i++) {
   //   console.log(middleLine);
     // console.log(nextLine);
