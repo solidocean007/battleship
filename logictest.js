fec5fd36753172c260cf1f0231d6c1c3de124da9
Clinton Williams <clintonwilliams007@gmail.com>
2023-01-16 14:21:37 -0500

    game play almost done for part-1

--- logictest.js
+++ logictest.js
@@ -1,15 +1,64 @@
-const results = [];
-let xCoord;
-
-let i = 0;
-while( i < 10) {
-  xCoord = Math.floor(Math.random() * (10) + 1)
-    if(results.indexOf(xCoord) === -1) {
-  results.push(xCoord);
-  i++
-    }
+var readlineSync = require('readline-sync');
+let endGame = false;
+do {
+  let shotInput;
+  while (shotInput !== 'R8'){
+    shotInput = readlineSync.question('Select letter and number R8:   ');
+    shotInput = shotInput.toLocaleUpperCase()
+    console.log(shotInput)
+  }
+  endGame = readlineSync.keyInYN('You have destroyed all battleships. Would you like to play again? Y/N')
+  console.log(endGame)
+  }  while (endGame)
   
-}
+
+
+
+// const boardSize = 3;
+// const boardLetters = ['A','B','C','D','E','F','G','H','I','J'];
+// const boardValues = ['1','2','3','4','5','6','7','8','9','10'];
+// const shotLocations = [];
+
+
+// let numberOfFoes = 2;
+// let enemyBoats = [];
+// let playerShot = '';
+// // this function creates enemy boats and assigns them a letter and a number.
+// function createEnemyTargets(totalTargets, enemyBoats){
+//   let shipCount = 0;
+//   let newShip;
+//   let xCoord;
+//   let yCoord;
+//   while(shipCount < totalTargets) { 
+//       xCoord = boardLetters[Math.floor(Math.random() * (boardSize) + 1)]
+//       // xCoord = boardLetters[xCoord];
+//       yCoord = Math.floor(Math.random() * (boardSize) + 1)
+//       newShip = (xCoord + yCoord); 
+//       if(enemyBoats.indexOf(newShip) === -1){
+//         enemyBoats.push(newShip);
+//         shipCount++
+//       }
+//     }  
+// }
+
+
+
+
+// console.log(createEnemyTargets(numberOfFoes, enemyBoats));
+
+
+// const results = [];
+// let xCoord;
+
+// let i = 0;
+// while( i < 10) {
+//   xCoord = Math.floor(Math.random() * (10) + 1)
+//     if(results.indexOf(xCoord) === -1) {
+//   results.push(xCoord);
+//   i++
+//     }
+  
+// }
 
 
 // console.log(results)
--- part_1.js
+++ part_1.js
@@ -1,20 +1,19 @@
-const boardSize = 3;
-let numberOfFoes = 2;
+const boardSize = 5;
 const boardLetters = ['A','B','C','D','E','F','G','H','I','J'];
-let enemyBoats = [];
 const boardValues = ['1','2','3','4','5','6','7','8','9','10'];
 const shotLocations = [];
-let playerShot = '';
+let endGame = false;
 
 // this function creates enemy boats and assigns them a letter and a number.
-function createEnemyTargets(totalTargets){
+function createEnemyTargets(totalTargets, enemyBoats){
   let shipCount = 0;
   let newShip;
   let xCoord;
+  let yCoord;
   while(shipCount < totalTargets) { 
-      xCoord = Math.floor(Math.random() * (boardSize) + 1)
-      xCoord = boardLetters[xCoord];
-      let yCoord = Math.floor(Math.random() * (3) + 1)
+      xCoord = boardLetters[Math.floor(Math.random() * (boardSize) + 1)]
+      // xCoord = boardLetters[xCoord];
+      yCoord = Math.floor(Math.random() * (boardSize) + 1)
       newShip = (xCoord + yCoord); 
       if(enemyBoats.indexOf(newShip) === -1){
         enemyBoats.push(newShip);
@@ -65,53 +64,47 @@ function buildGrid(size){
 }
 // This function gets the users shot at the enemy location.
 function takeYourShot(){
-  readlineSync.setDefaultOptions({limit: boardLetters});
-  xAnswer = (readlineSync.question('Enter a location to strike ie "A2" A - J'))
-    .toLocaleUpperCase();
-  readlineSync.setDefaultOptions({limit: boardValues});
-  yAnswer = readlineSync.question('1 - 10');
-  playerShot = String([xAnswer + yAnswer])
+  shotInput = readlineSync.question('Enter a location to strike ie "A2"   ');
+  playerShot = shotInput.toLocaleUpperCase()
+  if(shotLocations.includes(playerShot)){
+    console.log('You have already picked this location. Miss!');
+    takeYourShot();
+  }
   shotLocations.push(playerShot);
+  // console.log('playershot: ' + shotLocations)
 }
 
 
-//Game play begins here!!
-var readlineSync = require('readline-sync');
-// Wait for user response.
-if(readlineSync.keyIn('Press any key to start the game.   ')) {
-  }
-
-// Draw the board in the terminal.
-console.log(buildGrid(boardSize));
-
- // Create the enemy ships 
-createEnemyTargets(numberOfFoes);
-console.log(enemyBoats) // this line is just for testing
-
-
-while( numberOfFoes > 0 ){
-  takeYourShot()
-
-  if(enemyBoats.indexOf(playerShot) !== -1){
-    enemyBoats = enemyBoats.filter(element => element !== playerShot);
-    console.log(enemyBoats);
-    numberOfFoes--
-    console.log(`Hit. You have sunk a battleship. ${numberOfFoes} ship remaining.`)
-  } else {
-    console.log('miss me ha ha!');
-  }
-} console.log ('Game over!!')
-
-
-// ** But `promptCL` method should be used instead of this. **
-
-  // for(i = 0; i < boardLength; i++) {
-  //   console.log(middleLine);
-    // console.log(nextLine);
-    // console.log(nextLine);
-  // }
-
-
-
-
-
+  let numberOfFoes = 6;
+  let enemyBoats = [];
+  let playerShot = '';
+do{
+   //Game play begins here!!
+   var readlineSync = require('readline-sync');
+   // Wait for user response.
+   if(readlineSync.keyIn('Press any key to start the game.   ')) {
+     }
+ 
+   // Draw the board in the terminal.
+   console.log(buildGrid(boardSize));
+ 
+   // Create the enemy ships 
+   createEnemyTargets(numberOfFoes, enemyBoats);
+   console.log(enemyBoats) // this line is just for testing
+ 
+   while( numberOfFoes > 0 ){
+     takeYourShot()
+     if(enemyBoats.includes(playerShot)){  
+     console.log('playershot found') // this line is just for testing
+ 
+       enemyBoats = enemyBoats.filter(element => element !== playerShot);
+       console.log(enemyBoats);
+       numberOfFoes--
+       console.log(`Hit. You have sunk a battleship. ${numberOfFoes} ship remaining.`)
+     } else {
+       console.log('miss me ha ha!');
+     }
+   } 
+   endGame = readlineSync.keyInYN('You have destroyed all battleships. Would you like to play again? Y/N')
+   console.log(endGame)
+   }  while (endGame)
