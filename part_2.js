b4838937b298eeddf274e1ad983e78ab9ae1c7b4
solidocean007 <clintonwilliams007@gmail.com>
2023-01-30 20:41:46 -0500

    Done

--- part_2.js
+++ part_2.js
@@ -1,10 +1,13 @@
-const boardSize = 10;
+const size = 10;
 const boardLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-// const shotLocations = [];
-// let endGame = false;
+let board = gameBoardData(size);
+const shotLocations = [];
+let endGame = false;
 
-// This function creates a board.
-function buildGrid(size) {
+// Helper functions *******************************************
+
+// 1. This function draws the board in the terminal.
+function buildGrid(size, board) {
   // create board pieces without middle block
   const lineOnTop = " ______ ";
   const boxSpace = "|      |";
@@ -14,16 +17,12 @@ function buildGrid(size) {
   const topLine = lineOnTop.repeat(size);
   const nextLine = boxSpace.repeat(size);
 
-  for (let xPlotter = 0; xPlotter < size; xPlotter++) {
+  for (let i = 0; i < size; i++) {
     console.log(topLine);
     console.log(nextLine);
     let string = "";
-    for (let i = 0; i < size; i++) {
-      letterValue = boardLetters[xPlotter];
-      const board = [];
-      board[xPlotter] = { letterValue: xPlotter };
-
-      string += `|      |`;
+    for (let j = 0; j < size; j++) {
+      string += `|  ${board[i + j].damage}  |`;
     }
     console.log(string);
 
@@ -32,7 +31,7 @@ function buildGrid(size) {
   }
 }
 
-// This function builds data plots of the board.
+// 2. This function builds data plots of the board.
 function gameBoardData(size) {
   let gameBoard = [];
   for (let i = 0; i < size; i++) {
@@ -42,30 +41,31 @@ function gameBoardData(size) {
       gameBoard.push({
         id: xChar + yNumber,
         occupied: false,
+        damage: "  ",
       });
     }
   }
   return gameBoard;
 }
 
-/* This function return random 'vertical' or 'horizontal' */
+/* 3. This function return random 'vertical' or 'horizontal' */
 function whichWay() {
   theWay = Math.random() > 0.5 ? "vertical" : "horizontal";
   // console.log(theWay);
   return theWay;
 }
 
-// This function creates a random Letter for the x-axis
-function getLetter(){
-  return boardLetters[Math.floor(Math.random() * boardLetters.length)]
+// 4. This function creates a random Letter for the x-axis
+function getLetter() {
+  return boardLetters[Math.floor(Math.random() * boardLetters.length)];
 }
 
-// This function creates a random number
+// 5. This function creates a random number
 function getNumber() {
-  return Math.ceil(Math.random() * 10)
+  return Math.ceil(Math.random() * 10);
 }
 
-// This function gets the users shot at the enemy location.
+// 6. This function gets the users shot at the enemy location.
 function takeYourShot() {
   shotInput = readlineSync.question('Enter a location to strike ie "A2"   ');
   playerShot = shotInput.toLocaleUpperCase();
@@ -74,55 +74,54 @@ function takeYourShot() {
     takeYourShot();
   }
   shotLocations.push(playerShot);
-  console.log("playerShot: " + shotLocations);
+  return playerShot;
+}
+
+// If playerShot strikes boat remove that plot from boats array.
+function sinkSectionOfBoat(shot, enemyFleet, board) {
+  for (let i = 0; i < enemyFleet.length; i++) {
+    for (let j = 0; j < enemyFleet[i].length; j++) {
+      if (enemyFleet[i][j] === shot) {
+        enemyFleet[i].splice(j, 1);
+        console.log("You have hit a battleship!");
+        let hitSpot = board.find((obj) => obj.id === shot);
+        hitSpot.damage = "Hit";
+        buildGrid(size, board)
+        return;
+      }
+    }
+  }
+  console.log("You have missed!");
+}
+
+//This array checks if all ships are sunk
+function checkIfAllSunk(arr) {
+  let result = arr.every((ship) => ship.length === 0);
+  return result;
 }
 
 /* This function takes a spot, length and direction and builds 
   an array of possible locations */
-function buildArrayOfPlots(spot, length, direction){
-    let plots = [];
-    let xLetter = spot.slice(0,1);
-    let yNumber = Number(spot.slice(1));
-    if(direction === 'horizontal'){
-      for(let i = 0; i < length; i++){
-        plots.push(xLetter + Number(yNumber + i))
-      }
-      return plots;
-    } else {
-      let letterPlace = boardLetters.indexOf(xLetter);
-    for(let i = 0; i < length; i++){
-      plots.push(boardLetters[letterPlace + i] + yNumber)
+function buildArrayOfPlots(spot, length, direction) {
+  let plots = [];
+  let xLetter = spot.slice(0, 1);
+  let yNumber = Number(spot.slice(1));
+  if (direction === "horizontal") {
+    for (let i = 0; i < length; i++) {
+      plots.push(xLetter + Number(yNumber + i));
     }
     return plots;
+  } else {
+    let letterPlace = boardLetters.indexOf(xLetter);
+    for (let i = 0; i < length; i++) {
+      plots.push(boardLetters[letterPlace + i] + yNumber);
     }
-}
-
-// This function returns true if ship spots are real board spots
-function checkForRoom(shipSpots, realSpots) {
-  for (const plot of shipSpots) {
-    const actualSpot = realSpots.find(spot => spot.id === plot);
-    if (!actualSpot || actualSpot.occupied) {
-      return false;
-    } else {
-      actualSpot.occupied = true;
-    }
+    return plots;
   }
-
-  return true;
 }
 
-
-
-const size = boardSize;
-let board = gameBoardData(size)
-let allShipsArray = [];
-let shipFive = [];
-let shipFour = [];
-let shipThreeTwo = [];
-let shipThreeOne = [];
-let shipTwo = [];
-
-function shipBuilder(boatLength, ship) {
+// This function builds a ship.
+function shipBuilder(boatLength, ship, board) {
   let verticalOrHorizontal;
   let startSpot;
   do {
@@ -130,51 +129,56 @@ function shipBuilder(boatLength, ship) {
     startSpot = getLetter() + getNumber();
     ship = buildArrayOfPlots(startSpot, boatLength, verticalOrHorizontal);
   } while (!checkForRoom(ship, board));
-  console.log("ship: " + ship);
-  return ship
+  return ship;
 }
 
-shipFive = shipBuilder(5, shipFive)
-shipFour = shipBuilder(4, shipFour)
-shipThreeOne = shipBuilder(3, shipThreeOne)
-shipThreeTwo = shipBuilder(3, shipThreeTwo)
-shipTwo = shipBuilder(2, shipTwo)
+// This function returns true if ship spots are real board spots
+function checkForRoom(shipSpots, realSpots) {
+  for (const plot of shipSpots) {
+    const actualSpot = realSpots.find((spot) => spot.id === plot);
+    if (!actualSpot || actualSpot.occupied) {
+      return false;
+    } else {
+      actualSpot.occupied = true;
+    }
+  }
+  return true;
+}
 
-allShipsArray = [shipFive, shipFour, shipThreeOne, shipThreeTwo, shipTwo];
-
-console.log("gameBoardData with updated occupied values: ", board);
-console.log(allShipsArray)
-
-
-
-//Game play begins here!!
+// Create ships
 do {
+  let allShipsArray = [];
+  let shipFive = [];
+  let shipFour = [];
+  let shipThreeTwo = [];
+  let shipThreeOne = [];
+  let shipTwo = [];
+
+  shipFive = shipBuilder(5, shipFive, board);
+  shipFour = shipBuilder(4, shipFour, board);
+  shipThreeOne = shipBuilder(3, shipThreeOne, board);
+  shipThreeTwo = shipBuilder(3, shipThreeTwo, board);
+  shipTwo = shipBuilder(2, shipTwo, board);
+
+  allShipsArray = [shipFive, shipFour, shipThreeOne, shipThreeTwo, shipTwo];
+
+  console.log(allShipsArray);
+
+  //Game play begins here!! ******************************************
   var readlineSync = require("readline-sync");
   // Wait for user response.
   if (readlineSync.keyIn("Press any key to start the game.   ")) {
   }
   // Draw the board in the terminal.
-  console.log(buildGrid(boardSize));
+  console.log(buildGrid(size, board));
 
-  
-  while (numberOfFoes > 0) {
-    takeYourShot();
-    if (enemyBoats.includes(playerShot)) {
-      console.log("playerShot found"); // this line is just for testing
+  do {
+    let shot = takeYourShot();
+    sinkSectionOfBoat(shot, allShipsArray, board);
+    console.log(buildGrid(size, board));
+  } while (!checkIfAllSunk(allShipsArray));
 
-      enemyBoats = enemyBoats.filter((element) => element !== playerShot);
-      console.log(enemyBoats);
-      numberOfFoes--;
-      console.log(
-        `Hit. You have sunk a battleship. ${numberOfFoes} ship remaining.`
-      );
-    } else {
-      console.log("miss me ha ha!");
-    }
-  }
   endGame = readlineSync.keyInYN(
     "You have destroyed all battleships. Would you like to play again? Y/N"
   );
-  console.log(endGame);
 } while (endGame);
-
